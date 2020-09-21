70b345e79a6a6e9f37575f83d35ba05c8c87f05e
Jiwen <zhanjiwe@amazon.com>
2020-09-20 21:04:56 -0400

    add 0785 Is Graph Bipartite

---
+++ src/com/leetcode/jiwen/_0785_IsGraphBipartite.java
@@ -0,0 +1,40 @@
+package com.leetcode.jiwen;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _0785_IsGraphBipartite {
+
+	/**
+	 * Time: O(n + e)
+	 * Space: O(n)
+	 */
+	public boolean isBipartite(int[][] graph) {
+        int[] visited = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+        	if (visited[i] != 0) {
+        		continue;
+	        }
+        	Queue<Integer> q = new LinkedList<>();
+        	q.offer(i);
+        	visited[i] = 1; // Blue: 1; Red: -1.
+
+	        while (!q.isEmpty()) {
+	        	int cur = q.poll();
+	        	for (int neighbour : graph[cur]) {
+	        		if (visited[neighbour] == 0) {
+	        			// If this node hasn't been coloured,
+				        // Colour it with a different colour.
+				        visited[neighbour] = -visited[cur];
+				        q.offer(neighbour);
+			        } else if (visited[neighbour] != -visited[cur]) {
+	        			return false;
+			        }
+		        }
+	        }
+        }
+
+        return true;
+	}
+}
---
+++ tst/com/leetcode/jiwen/_0785_IsGraphBipartite_Test.java
@@ -0,0 +1,35 @@
+package com.leetcode.jiwen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class _0785_IsGraphBipartite_Test {
+
+	@Test
+	public void test1() {
+		final int[][] graph = new int[][]
+				{
+						{1, 3},
+						{0, 2},
+						{1, 3},
+						{0, 2}
+				};
+		final _0785_IsGraphBipartite myClass = new _0785_IsGraphBipartite();
+		assertTrue(myClass.isBipartite(graph));
+	}
+
+	@Test
+	public void test2() {
+		final int[][] graph = new int[][]
+				{
+						{1, 2, 3},
+						{0, 2},
+						{0, 1, 3},
+						{0, 2}
+				};
+		final _0785_IsGraphBipartite myClass = new _0785_IsGraphBipartite();
+		assertFalse(myClass.isBipartite(graph));
+	}
+}
