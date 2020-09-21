917dc721dcaa19ee446316ff18425f41ed1727d8
Jiwen <zhanjiwe@amazon.com>
2020-09-20 21:12:08 -0400

    Update 785

--- tst/com/leetcode/jiwen/_0785_IsGraphBipartite_Test.java
+++ tst/com/leetcode/jiwen/_0785_IsGraphBipartite_Test.java
@@ -7,29 +7,29 @@ import static org.junit.Assert.assertTrue;
 
 public class _0785_IsGraphBipartite_Test {
 
-	@Test
-	public void test1() {
-		final int[][] graph = new int[][]
-				{
-						{1, 3},
-						{0, 2},
-						{1, 3},
-						{0, 2}
-				};
-		final _0785_IsGraphBipartite myClass = new _0785_IsGraphBipartite();
-		assertTrue(myClass.isBipartite(graph));
-	}
+    @Test
+    public void test1() {
+        final int[][] graph = new int[][]
+                {
+                        {1, 3},
+                        {0, 2},
+                        {1, 3},
+                        {0, 2}
+                };
+        final _0785_IsGraphBipartite myClass = new _0785_IsGraphBipartite();
+        assertTrue(myClass.isBipartite(graph));
+    }
 
-	@Test
-	public void test2() {
-		final int[][] graph = new int[][]
-				{
-						{1, 2, 3},
-						{0, 2},
-						{0, 1, 3},
-						{0, 2}
-				};
-		final _0785_IsGraphBipartite myClass = new _0785_IsGraphBipartite();
-		assertFalse(myClass.isBipartite(graph));
-	}
+    @Test
+    public void test2() {
+        final int[][] graph = new int[][]
+                {
+                        {1, 2, 3},
+                        {0, 2},
+                        {0, 1, 3},
+                        {0, 2}
+                };
+        final _0785_IsGraphBipartite myClass = new _0785_IsGraphBipartite();
+        assertFalse(myClass.isBipartite(graph));
+    }
 }
