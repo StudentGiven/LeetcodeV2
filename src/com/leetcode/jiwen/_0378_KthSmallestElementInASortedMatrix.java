f4d1fb67602f5246502eed7e9bfc07379982e9a1
Jiwen <given209@terpmail.umd.edu>
2020-09-24 23:49:11 -0400

    Add 378 Kth Smallest Element In A Sorted Matrix

--- src/com/leetcode/jiwen/_0378_KthSmallestElementInASortedMatrix.java
+++ src/com/leetcode/jiwen/_0378_KthSmallestElementInASortedMatrix.java
@@ -1,8 +1,46 @@
 package com.leetcode.jiwen;
 
+import java.util.PriorityQueue;
+
 public class _0378_KthSmallestElementInASortedMatrix {
 
+	class Cell implements Comparable<Cell> {
+		int row;
+		int column;
+		int value;
+
+		Cell(int row, int column, int value) {
+			this.row = row;
+			this.column = column;
+			this.value = value;
+		}
+
+		@Override
+		public int compareTo(Cell o) {
+			return value - o.value;
+		}
+	}
+
 	public int kthSmallest(int[][] matrix, int k) {
-		return 0;
+		int n = matrix.length;
+		PriorityQueue<Cell> queue = new PriorityQueue<>();
+		boolean[][] visited = new boolean[n][n];
+		queue.offer(new Cell(0, 0, matrix[0][0]));
+		visited[0][0] = true;
+
+		for (int i = 0; i < k - 1; i++) {
+			Cell cur = queue.poll();
+			if (cur.row + 1 < n && !visited[cur.row + 1][cur.column]) {
+				queue.offer(new Cell(cur.row + 1, cur.column,
+						matrix[cur.row + 1][cur.column]));
+				visited[cur.row + 1][cur.column] = true;
+			}
+			if (cur.column + 1 < n && !visited[cur.row][cur.column + 1]) {
+				queue.offer(new Cell(cur.row, cur.column + 1,
+						matrix[cur.row][cur.column + 1]));
+				visited[cur.row][cur.column + 1] = true;
+			}
+		}
+		return queue.peek().value;
 	}
 }
--- tst/com/leetcode/jiwen/_0378_KthSmallestElementInASortedMatrix_Test.java
+++ tst/com/leetcode/jiwen/_0378_KthSmallestElementInASortedMatrix_Test.java
@@ -1,6 +1,21 @@
 package com.leetcode.jiwen;
 
-public class _0378_KthSmallestElementInASortedMatrix_Test {
-	
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class _0378_KthSmallestElementInASortedMatrix_Test {
+
+	@Test
+	public void test1() {
+		final int[][] matrix = new int[][]
+				{
+						{ 1,  5,  9},
+						{10, 11, 13},
+						{12, 13, 15}
+				};
+		final _0378_KthSmallestElementInASortedMatrix myClass =
+				new _0378_KthSmallestElementInASortedMatrix();
+		assertEquals(13, myClass.kthSmallest(matrix, 8));
+	}
 }
