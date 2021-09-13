691c2b24dfc17e0dd0ab3a6af59fd662208af437
Jiwen <given209@terpmail.umd.edu>
2021-09-12 20:35:55 -0400

    Add 0035

---
+++ src/com/leetcode/jiwen/_0035_SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package com.leetcode.jiwen;
+
+public class _0035_SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
