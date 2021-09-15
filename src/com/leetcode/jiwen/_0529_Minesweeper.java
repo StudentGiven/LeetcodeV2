aea5a6e314b13bb9ca94294a74a6791fd5e51415
Jiwen <given209@terpmail.umd.edu>
2021-09-14 23:16:26 -0400

    Add 529 DFS

---
+++ src/com/leetcode/jiwen/_0529_Minesweeper.java
@@ -0,0 +1,46 @@
+package com.leetcode.jiwen;
+
+public class _0529_Minesweeper {
+
+    // DFS
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int m = board.length;
+        int n = board[0].length;
+
+        int row = click[0];
+        int col = click[1];
+
+        if (board[row][col] == 'M') {
+            board[row][col] = 'X';
+        } else { // Empty
+            // Get number of mines first.
+            int count = 0;
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int r = row + i, c = col + j;
+                    if (r < 0 || r >= m || c < 0 || c >= n) continue;
+                    if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                }
+            }
+
+            if (count > 0) { // If it is not a 'B', stop further DFS.
+                board[row][col] = (char)(count + '0');
+            }
+            else { // Continue DFS to adjacent cells.
+                board[row][col] = 'B';
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c >= n) continue;
+                        if (board[r][c] == 'E') updateBoard(board, new int[] {r, c});
+                    }
+                }
+            }
+        }
+
+        return board;
+
+    }
+}
