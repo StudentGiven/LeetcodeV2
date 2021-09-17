232986f7fdbc42620f73433ec7c3ff7ca40f7251
Jiwen <given209@terpmail.umd.edu>
2021-09-17 18:00:21 -0400

    Add Find Min Bug question

---
+++ src/com/juyingoa/jiwen/_002_FindMinBug.java
@@ -0,0 +1,48 @@
+package com.juyingoa.jiwen;
+
+public class _002_FindMinBug {
+
+    /**
+     * The following find_min function should return the smallest integer from a given array A.
+     *
+     * int find_min(int[] A) {
+     *     int ans = 0;
+     *     for (int i = 1; i < A.length; i++) {
+     *         if (ans > A[i]) {
+     *             ans = A[i];
+     *         }
+     *     }
+     *     return ans;
+     * }
+     *
+     * Unfortunately it is an incorrect implementation. In other words, when the function is called with certain parameters, it returns the wrong
+     * answer. Your task is to generate a counterexample, i.e. an array A consisting of N integers such that the find_min function returns the
+     * wrong answer.
+     *
+     * Write a function:
+     *     class Solution { public int[] solution(int N); }
+     * that, given an integer N, returns an array A consisting of N integers which describes a counterexample.
+     *
+     * Example:
+     * Given N = 4, your function may return [4, 2, 4, 5]. It is a counterexample, because calling the find_min function with this array returns 0, but
+     * the correct answer is 2. Your function may also return another counterexample; for example, [10, 567, 99, 456].
+     *
+     * Assume that:
+     *     · N is an integer within the range [1 ... 1,000].
+     */
+
+     public int find_min(int[] A) {
+         int ans = 0;
+         for (int i = 1; i < A.length; i++) {
+             if (ans > A[i]) {
+                 ans = A[i];
+             }
+         }
+         return ans;
+     }
+
+     public int[] solution(int N) {
+
+     }
+
+}
