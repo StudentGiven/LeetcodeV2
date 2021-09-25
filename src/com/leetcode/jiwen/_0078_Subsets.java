3b210bc5f2981e8c6deadf8981be5aa0eaf14497
Jiwen <given209@terpmail.umd.edu>
2021-09-25 17:12:18 -0400

    Add subset dfs iteration

--- src/com/leetcode/jiwen/_0078_Subsets.java
+++ src/com/leetcode/jiwen/_0078_Subsets.java
@@ -2,26 +2,27 @@ package com.leetcode.jiwen;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class _0078_Subsets {
 
-	public List<List<Integer>> subsets(int[] nums) {
+	public List<List<Integer>> subsets_dfs_recursion(int[] nums) {
 		List<List<Integer>> result = new ArrayList<>();
 		if (nums == null || nums.length == 0) {
 			return result;
 		}
 
 		List<Integer> current = new ArrayList<>(); // record the current subset
-		dfs(nums, current, 0, result);
+		dfs_recursion(nums, current, 0, result);
 
 		return result;
 	}
 
 	// At each level, determine the integer at current index to be picked or not
-	public void dfs(int[] nums,
-					List<Integer> current,
-					int index,
-					List<List<Integer>> result) {
+	private void dfs_recursion(int[] nums,
+							  List<Integer> current,
+					          int index,
+							  List<List<Integer>> result) {
 		// Terminate condition: when the index reaches to the input end
 		if (index == nums.length) {
 			result.add(new ArrayList<>(current));
@@ -30,11 +31,37 @@ public class _0078_Subsets {
 
 		// Pick
 		current.add(nums[index]);
-		dfs(nums, current, index + 1, result);
+		dfs_recursion(nums, current, index + 1, result);
 		current.remove(current.size() - 1);
 
 		// Not pick
-		dfs(nums, current, index + 1, result);
+		dfs_recursion(nums, current, index + 1, result);
 
+	}
+
+	public List<List<Integer>> subsets_dfs_iteration(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+
+		List<Integer> current = new ArrayList<>();
+		Stack<Integer> stack = new Stack<>();
+		stack.push(0);
+
+		while (!stack.isEmpty()) {
+			int index = stack.pop();
+			if (index == nums.length) {
+				result.add(new ArrayList<>(current));
+				if (!current.isEmpty()) {
+					current.remove(current.size() - 1);
+				}
+				continue;
+			}
+			stack.push(index + 1);
+			current.add(nums[index]);
+			stack.push(index + 1);
+		}
+		return result;
 	}
 }
