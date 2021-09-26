99af0f1244f8830ebfcf70668a567ebabd6894b8
Jiwen <given209@terpmail.umd.edu>
2021-09-25 20:10:41 -0400

    Implement ValidParentheses

--- src/com/leetcode/jiwen/_0020_ValidParentheses.java
+++ src/com/leetcode/jiwen/_0020_ValidParentheses.java
@@ -1,5 +1,26 @@
 package com.leetcode.jiwen;
 
+import java.util.Stack;
+
 public class _0020_ValidParentheses {
-    // Test
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == '(' || current == '[' || current == '{') {
+                stack.push(current);
+            } else if (current == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+                return false;
+            } else if (current == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+                return false;
+            } else if (current == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
