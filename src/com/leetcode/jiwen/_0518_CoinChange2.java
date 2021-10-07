66903558f665e972982122707d52fc72a67550ff
Jiwen <given209@terpmail.umd.edu>
2021-10-07 15:53:45 -0400

    Update 518

--- src/com/leetcode/jiwen/_0518_CoinChange2.java
+++ src/com/leetcode/jiwen/_0518_CoinChange2.java
@@ -1,6 +1,7 @@
 package com.leetcode.jiwen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _0518_CoinChange2 {
@@ -31,60 +32,35 @@ public class _0518_CoinChange2 {
         }
     }
 
-    /**
-     *     public int change(int amount, int[] coins) {
-     *
-     *         to avoid duplicated count for combination to reach amount, avoid to repeated calcualte [1,2] and [2,1]
-     *         e.x., coins = {1,2}, amount = 3
-     *             comb = [1,1,1], [1,2]
-     *         need to loop coins first, which means if 2 has been used in one path, it cannot be used again, need to have a way to record which coins has been used and cannot be used again
-     *                              3
-     *                        (1)/     \ (2): pass 1 already, so 1 cannot be used again in any branch below
-     *                          2        1 (cannot go further)
-     *                     (1)/   \(2)
-     *                       1     0
-     *                   (1)/
-     *                     0
-     *
-     *         if (amount == 0) {
-     *              return 1;
-     *         } else if (coins == null || coins.length == 0) {
-     *              return 0;
-     *         }
-     *         int[][] memo = new int[coins.length][amount + 1];
-     *         /**use index to keep track which coin has been used
-     *         memo[i][j]: # of combination to reach j with coin choices[0, i], so going forward
-     *         1. keep using coins[i]
-     *             memo[i][j] = memo[i][j - coins[i]];
-     *         2. skip coins[i] and use coins[i + 1]
-     *             memo[i][j] = memo[i + 1][j]
-     *         *
-             *for(
-    int[] m :memo)
+    public int change2(int amount, int[] coins) {
+        int[][] memo = new int[coins.length][amount + 1];
+        for (int[] m : memo) {
+            Arrays.fill(m, -1);
+        }
 
-    {
-     *Arrays.fill(m, -1);
-     *}
-     *return
+        return dfs2(coins, coins.length - 1, amount, memo);
+    }
 
-    dfs(coins, coins.length -1, amount, memo);
-     *
-}
-     *private int dfs(int[]coins,int index,int amount,int[][]memo){
-        *if(amount==0){
-        *return 1;
-        *}else if(index==-1){
-        *return 0;
-        *}else if(memo[index][amount]!=-1){
-        *return memo[index][amount];
-        *}
-        *int result=0;
-        *if(coins[index]<=amount){
-        *result+=dfs(coins,index,amount-coins[index],memo);
-        *}
-        *result+=dfs(coins,index-1,amount,memo);
-        *memo[index][amount]=result;
-        *return result;
-        *}
-     */
+    private int dfs2(int[] coins, int index, int amount, int[][] memo) {
+        if (amount == 0) {
+            return 1;
+        }
+
+        if (index == -1) {
+            return 0;
+        }
+
+        if (memo[index][amount] != -1) {
+            return memo[index][amount];
+        }
+
+        int result = 0;
+        if (coins[index] <= amount) {
+            result += dfs2(coins, index, amount - coins[index], memo);
+        }
+
+        result += dfs2(coins, index - 1, amount, memo);
+        memo[index][amount] = result;
+        return result;
+    }
 }
--- tst/com/leetcode/jiwen/_0518_CoinChange2_Test.java
+++ tst/com/leetcode/jiwen/_0518_CoinChange2_Test.java
@@ -6,6 +6,6 @@ public class _0518_CoinChange2_Test {
         _0518_CoinChange2 myClass = new _0518_CoinChange2();
         int amount = 500;
         int[] coins = new int[]{3, 5, 7, 8, 9, 10, 11};
-        System.out.println(myClass.change(amount, coins));
+        System.out.println(myClass.change2(amount, coins));
     }
 }
